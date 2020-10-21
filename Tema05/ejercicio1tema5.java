d7f74b33cbbf2bdbcae848eeb7bb19b68c95ee54
guillelj12 <72443418+guillelj12@users.noreply.github.com>
2020-10-21 23:03:23 +0200

    Add files via upload

---
+++ Tema05/ejercicio1tema5.java
@@ -0,0 +1,8 @@
+public class ejercicio1tema5 {
+  public static void main (String[] args) {
+  //Escrito por Guillermo López Jiménez.
+    for(int i =0;i<=100;i+=5){
+      System.out.println(i);
+    }
+  }
+}
---
+++ Tema05/ejercicio4tema5.java
@@ -0,0 +1,8 @@
+public class ejercicio4tema5 {
+  public static void main (String[] args) {
+  // Escrito por Guillermo López Jiménez.
+    for(int i=320;i>=160;i=i-20) {
+      System.out.println(i);
+    }
+  }
+}
